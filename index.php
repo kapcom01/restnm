932d0b63b5ef4d73755852cb253801baaae3ec6d
Emmanouil Kapernaros <manolis@kapcom.gr>
2017-08-19 10:46:54 +0000

    print different table for each switch

--- index.php
+++ index.php
@@ -7,22 +7,21 @@
 <div class="panel panel-default">
   <!-- Default panel contents -->
   <div class="panel-heading">Manolis SNMP in PHP test</div>
-  <table class="table">
 
 <?php
 include 'snmp_work.php';
-
 $ini_array = parse_ini_file("switches.ini");
+
 foreach ($ini_array['ip'] as $ip) {
-        print("<p> Refreshing SNMP of $ip ...</p>");
+        print("<p>Switch ($ip):</p>");
 	$if2mac = ifname_mac($ip);
+	print("<table>");
 	foreach ($if2mac as $interface => $mac) {
 		print "<tr><td>$interface</td><td>$mac</td></tr>";
 	}
+	print("</table>");
 }
-
 ?>
-  </table>
 </div>
 </body>
 </html>
