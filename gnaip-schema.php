742b5f339d84ce386bab69353fa93b4f08272906
Emmanouil Kapernaros <manolis@kapcom.gr>
2018-03-18 17:18:40 +0000

    gnaip-schema and index table fixes

--- gnaip-schema.php
+++ gnaip-schema.php
@@ -17,22 +17,22 @@ function orofos($ip_address){
   if ($ip_byte[0] != '172' || $ip_byte[1] != '16') return '';
 
   if (strlen($ip_byte[2]) > 2)
-    $orofos = substr($ip_byte[2], 2, 1);
+    $orofos = substr($ip_byte[2], 2, 1)-1;
   else
-    $orofos = substr($ip_byte[2], 1, 1);
+    $orofos = substr($ip_byte[2], 1, 1)-1;
 
   return $orofos;
 }
 
-function type($ip_address){
+function typos($ip_address){
   $ip_byte = explode(".",$ip_address);
   if ($ip_byte[0] != '172' || $ip_byte[1] != '16') return '';
 
-  if ($ip_byte[3] < 100) $type = 'pc';
-  elseif ($ip_byte[3] < 200) $type = 'printer';
-  else $type = 'other_device';
+  if ($ip_byte[3] < 100) $typos = 'pc';
+  elseif ($ip_byte[3] < 200) $typos = 'printer';
+  else $typos = 'other_device';
 
-  return $type;
+  return $typos;
 }
 
 ?>
--- index.php
+++ index.php
@@ -4,6 +4,10 @@
 </head>
 <body>
 
+<p>
+<table border="1">
+<tr><td><b>Switch IP</b></td><td><b>Switch Ktirio</b></td><td><b>Switch Orofos</b></td><td><b>Switch Port</b></td><td><b>Device MAC Address</b></td><td><b>Devive IP Address</b></td><td><b>Device Type</b></td><td><b>Device Ktirio</b></td><td><b>Device Orofos</b></td><td><b>Device Vendor</b></td></tr>
+
 <?php
 include 'snmp_work.php';
 include 'arp_work.php';
@@ -11,49 +15,35 @@ include 'gnaip-schema.php';
 
 $ini_array = parse_ini_file("switches.ini");
 
-
 foreach ($ini_array['ip'] as $ip) {
-$swports = snmp_swports($ip);
-$arptable = get_mac_ip_vendor();
+  $swports = snmp_swports($ip);
+  $arptable = get_mac_ip_vendor();
 
-  foreach ($arptable as $arp_row) {
-    if(strcmp($arp_row['ip_address'],$ip)==0)
-    {  $swvendor = $arp_row['vendor'];}
-  }
-?>
-
-<p>
-<table border="1">
-<tr><td colspan=10 align=center><b> <?php echo $swvendor . " (" .$ip. ")"; ?> </b></td></tr>
-<tr><td><b>switch ip</b></td><td><b>switch ktirio</b></td><td><b>switch orofos</b></td><td><b>switch port</b></td><td><b>MAC Address</b></td><td><b>IP Address</b></td><td><b>IP ktirio</b></td><td><b>IP orofos</b></td><td><b>Vendor</b></td></tr>
-
-<?php
   $tr_bgcolor = "#BDBDBD";
+
   // iterate snmp data
   foreach ($swports as $swport) {
     if (count($swport['mac_address'])) {
       $tr_bgcolor = ($tr_bgcolor=="#FFFFFF") ? "#BDBDBD" : "#FFFFFF";
       foreach ($swport['mac_address'] as $row_mac_address) {
-        echo "<tr bgcolor=" . $tr_bgcolor . "><td>" . $ip . "</td><td></td><td></td><td>" . $swport['ifname'] . "</td><td>" . $row_mac_address . "</td><td>" . $arptable[$row_mac_address]['ip_address'] . "</td><td>" . ktirio($arptable[$row_mac_address]['ip_address'])  . "</td><td>" . orofos($arptable[$row_mac_address]['ip_address'])  . "</td><td>" . $arptable[$row_mac_address]['vendor'] . "</td></tr>";
+        echo "<tr bgcolor=" . $tr_bgcolor . "><td>" . $ip . "</td><td>" . ktirio($ip) . "</td><td>" . orofos($ip) . "</td><td>" . $swport['ifname'] . "</td><td>" . $row_mac_address . "</td><td>" . $arptable[$row_mac_address]['ip_address'] . "</td><td>" . typos($arptable[$row_mac_address]['ip_address']) . "</td><td>" . ktirio($arptable[$row_mac_address]['ip_address'])  . "</td><td>" . orofos($arptable[$row_mac_address]['ip_address'])  . "</td><td>" . $arptable[$row_mac_address]['vendor'] . "</td></tr>";
         echo "\n";
         unset($arptable[$row_mac_address]);
         }
       }
-    } 
+    }
 
   // iterate arp data
   foreach (array_keys($arptable) as $arp_mac) {
     $tr_bgcolor = ($tr_bgcolor=="#FFFFFF") ? "#BDBDBD" : "#FFFFFF";
-    echo "<tr bgcolor=" . $tr_bgcolor . "><td>" . $ip . "</td><td></td><td></td><td></td><td>" . $arp_mac . "</td><td>" . $arptable[$arp_mac]['ip_address'] . "</td><td>" . ktirio($arptable[$arp_mac]['ip_address'])  . "</td><td>" . orofos($arptable[$arp_mac]['ip_address'])  . "</td><td>" . $arptable[$arp_mac]['vendor'] . "</td></tr>";
+    echo "<tr bgcolor=" . $tr_bgcolor . "><td>" . $ip . "</td><td>" . ktirio($ip) . "</td><td>" . orofos($ip) . "</td><td></td><td>" . $arp_mac . "</td><td>" . $arptable[$arp_mac]['ip_address'] . "</td><td>" . typos($arptable[$arp_mac]['ip_address']) . "</td><td>" . ktirio($arptable[$arp_mac]['ip_address'])  . "</td><td>" . orofos($arptable[$arp_mac]['ip_address'])  . "</td><td>" . $arptable[$arp_mac]['vendor'] . "</td></tr>";
     echo "\n";
   }
-?>
-</table>
-</p>
-
-<?php
 }
 ?>
+
+</table>
+</p>
 
 </body>
 </html>
